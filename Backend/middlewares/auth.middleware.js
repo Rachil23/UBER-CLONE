7580e9ca97cd6dcdf46a8e116c5ec744070211dd
Rachil23 <rachilnamdev72@gmail.com>
2025-02-07 14:51:36 +0530

    Login and logout of ride owner created and minor bug fixed

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blacklistTokenModel = require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.model');
 
 
 module.exports.authUser = async (req, res, next) => {
@@ -11,7 +13,7 @@ module.exports.authUser = async (req, res, next) => {
 
     }
 
-    const isBlacklisted = await userModel.findOne({token: token});
+    const isBlacklisted = await blacklistTokenModel.findOne({token: token});
 
     if(isBlacklisted) {
         return res.status(401).json({ message: 'unauthorized' });
@@ -28,5 +30,31 @@ module.exports.authUser = async (req, res, next) => {
 
     }catch(err){
         return res.status(401).json({ message:"Unauthorized"});
+    }
+}
+
+module.exports.authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    
+    if(!token){
+        return res.status(401).json({ message: "Unauthorized"}); 
+    }
+    const isBlacklisted = await blacklistTokenModel.findOne({ token: token });
+
+    if (isBlacklisted){
+        return res.status(401).json({ message: "Unauthorized"}); 
+    }
+
+    try{
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+        req.captain = captain;
+        return next();
+
+    }catch(err) {
+
+        res.status(401).json({ message: "Unauthorized"});
+
     }
 }
--- Backend/models/blacklistToken.model.js
+++ Backend/models/blacklistToken.model.js
@@ -15,4 +15,4 @@ const blacklistTokenSchema = new mongoose.Schema({
 
 });
 
-module.exports - mongoose.model('BlacklistToken', blacklistTokenSchema);
+module.exports = mongoose.model('BlacklistToken', blacklistTokenSchema);
