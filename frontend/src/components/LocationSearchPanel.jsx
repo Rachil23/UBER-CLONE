63d47ae9d2a02bd25d842958643ba6425f5c63a2
Rachil23 <rachilnamdev72@gmail.com>
2025-02-20 19:29:06 +0530

    Home page after login and ride location UI implementation

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,39 @@
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        <div className="flex gap-3 items-center my-4 justify-start">
+            <h2 className="bg-gray-300 h-8 w-12 flex items-center justify-center rounded-full">
+                <i className="ri-map-pin-fill"></i>
+            </h2>
+            <h4 className="font-medium">
+                4-158/9 5th Cross Road Sainikpuri Secunderabad 
+                500094, TS IND 
+            </h4>
+        </div>
+
+        <div className="flex gap-3 items-center my-4 justify-start">
+            <h2 className="bg-gray-300 h-8 w-12 flex items-center justify-center rounded-full">
+                <i className="ri-map-pin-fill"></i>
+            </h2>
+            <h4 className="font-medium">
+                4-158/9 5th Cross Road Sainikpuri Secunderabad 
+                500094, TS IND 
+            </h4>
+        </div>
+
+        <div className="flex gap-3 items-center my-4 justify-start">
+            <h2 className="bg-gray-300 h-8 w-12 flex items-center justify-center rounded-full">
+                <i className="ri-map-pin-fill"></i>
+            </h2>
+            <h4 className="font-medium">
+                4-158/9 5th Cross Road Sainikpuri Secunderabad 
+            500094, TS IND
+            </h4>
+        </div>
+
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,107 @@
 /* eslint-disable no-unused-vars */
+import { useState } from "react"
 import React from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from "gsap";
+import { useRef } from "react";
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from "../components/LocationSearchPanel";
 
-const Home = () => {
+const Home = () => { 
+
+  const [pickup, setPickup] = useState('')
+  const [destination, setDestination] = useState('')
+  const [panelOpen, setPanelOpen] = useState(false)
+  const panelRef = useRef(null)
+  const panelCloseRef = useRef(null)
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  useGSAP(function() {
+    if(panelOpen){
+      gsap.to(panelRef.current,{
+        height: '75%',
+        padding:24
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:1
+        
+      })
+    }else{
+      gsap.to(panelRef.current,{
+        height: '0%',
+        padding:0
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:0
+      })
+    }
+  },[panelOpen])
+
   return (
-    <div>Home</div>
+    <div className='h-screen relative'>
+      <img className=" w-18 mb-6 absolute left-7 top-7 " 
+      src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"/>
+
+      <div className='h-screen w-screen'>
+
+        <img className='h-full w-full object-cover' 
+        src="https://w0.peakpx.com/wallpaper/336/971/HD-wallpaper-usa-map-top-best-quality-usa-map-background-aesthetic-map-thumbnail.jpg" />
+
+      </div>
+
+      <div className=' absolute flex flex-col justify-end top-0 h-screen w-full'>
+
+        <div className='h-[25%] bg-white p-3.5 rounded-t-xl relative'>
+          <h5 ref={panelCloseRef} onClick={ ()=> 
+            setPanelOpen(false) } 
+          className='absolute top-3 right-3 text-2xl'>
+            <i className="ri-arrow-down-s-line "></i>
+          </h5>
+
+          <h4 className='text-3xl font-semibold mb-3'>Find a trip ?</h4>
+
+            <form onSubmit={(e) => {
+              submitHandler(e);
+            }}>
+
+            <div className="line absolute h-16 w-1 top-[45%] left-7 bg-gray-800 rounded-full "></div>
+
+            <input
+            onClick={(e) => {
+              setPanelOpen(true)
+            }} 
+            value={pickup}
+            onChange={(e) => {
+              setPickup(e.target.value)
+            }}
+            className='bg-[#eee] px-9 py-2.5 text-lg rounded-lg w-full mb-3' 
+            type='text'  
+            placeholder='Add a pick-up location'/>
+
+            <input
+            onClick={(e) => {
+              setPanelOpen(true)
+            }} 
+            value={destination}
+            onChange={(e) => {
+              setDestination(e.target.value)
+            }} 
+            className='bg-[#eee] px-9 py-2.5 text-lg rounded-lg w-full mb-1 ' 
+            type='text'  
+            placeholder='Enter your destination'/>
+
+          </form>
+
+        </div>
+
+        <div ref={panelRef} className='h-[0] bg-gray-100 '>
+            <LocationSearchPanel/>
+        </div>
+      </div>
+    </div>
   )
 }
 
